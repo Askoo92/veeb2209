ab3e58925e112d7cb695fa7d8dbdadcf09dfa20b
Askoo92 <asko.otsalt@gmail.com>
2022-10-13 14:51:40 +0300

    Loeng07

---
+++ Loeng07/script.js
@@ -0,0 +1,10 @@
+let astendaja = 0;
+
+console.log('Alustame astendamisega');
+
+while (astendaja < 10) {
+    console.log('2 astmes ' + astendaja + ' võrdub ' + 2 ** astendaja);
+    astendaja = astendaja + 1
+}
+
+console.log('Lõpetame astendamisega');
